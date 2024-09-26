4cfd8483da9fd18900e3d5dcabec1bec8369633b
DuydoFE <manhlon18@gmail.com>
2024-09-26 14:50:34 +0700

    LoginGoogleFireBase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // just change config below to start
 const firebaseConfig = {
-  apiKey: "AIzaSyBY1PxLeYzNdL-mzV9x1cJzPIF66UNBqkg",
-  authDomain: "swp-asp.firebaseapp.com",
-  projectId: "swp-asp",
-  storageBucket: "swp-asp.appspot.com",
-  messagingSenderId: "840654227120",
-  appId: "1:840654227120:web:f86f024ab8f4ad9e755d75",
-  measurementId: "G-X2L1D29BGG",
+  apiKey: "AIzaSyBYmYaayYgHH0IUY_F9C1ho1aX7naMlcg4",
+  authDomain: "fedutoy.firebaseapp.com",
+  projectId: "fedutoy",
+  storageBucket: "fedutoy.appspot.com",
+  messagingSenderId: "993544393754",
+  appId: "1:993544393754:web:1144b8ed1c8a867f952da3",
+  measurementId: "G-K6JZQHSTJC"
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
+
+export const ggProvider = new GoogleAuthProvider()
