211f3dcc89873853a359758c5f28e268c91bd551
DuydoFE <manhlon18@gmail.com>
2024-09-30 13:31:52 +0700

    LoginApi

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <ToastContainer />
+  </>
+);
--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,4 +1,4 @@
-import Login from "../components/login/login";
+import Login from "../components/login/Login";
 
 function LoginPage() {
   return <Login />;
