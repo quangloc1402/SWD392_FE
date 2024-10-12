f3ea083ebda248d2ba67cfbc3dfdacbd4af43dea
DuydoFE <manhlon18@gmail.com>
2024-10-12 11:54:25 +0700

    ProtectRoute

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -8,6 +8,18 @@ import HomePage from "../components/homepage";
 import ManageStaff from "../page/admin/manage-staff";
 import ManageUser from "../page/admin/manage-user";
 import ManageCategory from "../page/admin/manage-category";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
+const ProtectRouteAuth = ({ children }) => {
+  const user = useSelector((store) => store.user);
+  console.log(user);
+  if (user && user?.role === "ADMIN") {
+    return children;
+  }
+  toast.error("Ai cho may vo ? Cút !! ");
+  return <Navigate to={"/"} />;
+};
 export const router = createBrowserRouter([
   {
     path: "/Login",
@@ -19,7 +31,11 @@ export const router = createBrowserRouter([
   },
   {
     path: "/Dashboard",
-    element: <Dashboard />,
+    element: (
+      <ProtectRouteAuth>
+        <Dashboard />
+      </ProtectRouteAuth>
+    ),
     children: [
       {
         path: "managestaff",
