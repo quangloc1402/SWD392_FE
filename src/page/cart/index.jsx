59f03144c98fcf91a3019f1c95e7c17be7d39e1e
DuydoFE <manhlon18@gmail.com>
2024-10-22 21:41:52 +0700

    FixConFlict

--- src/page/cart/index.jsx
+++ src/page/cart/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { clearAll } from "../../redux/features/cartSlice";
+import { toast } from "react-toastify";
 function CartPage() {
   const columns = [
     {
@@ -44,6 +45,24 @@ function CartPage() {
     onChange: onSelectChange,
   };
 
+  const handleBuy = async () => {
+    try {
+      const seletedItems = data.filter((toy) =>
+        selectedRowKeys.includes(toy.id)
+      );
+      console.log(seletedItems);
+
+      const item = selectedRowKeys.map((toy) => ({
+        postId: toy.id,
+        quantity: toy.quantity,
+      }));
+      const response = await api.post("order/buy", { item });
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to create order");
+    }
+  };
+
   return (
     <div
       style={{
@@ -51,7 +70,14 @@ function CartPage() {
       }}
     >
       <Button onClick={() => dispatch(clearAll())}>Clear All</Button>
+      <Table
+        rowKey="id"
+        rowSelection={rowSelection}
+        columns={columns}
+        dataSource={data}
+      />
 
+      <Button onClick={handleBuy}>Buy </Button>
     </div>
   );
 }
