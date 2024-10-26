b666c191ae9e1027662d9ff3d76294dbf14b9238
unknown <manhlon18@gmail.com>
2024-10-27 05:55:40 +0700

    FeedBack

--- src/page/history/index.jsx
+++ src/page/history/index.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import api from "../../config/axios";
-import { Table } from "antd";
-
+import { Alert, Button, Form, Input, Modal, Rate, Table } from "antd";
+import { render } from "react-dom";
+import { useForm } from "antd/es/form/Form";
+import { toast } from "react-toastify";
 function History() {
   const [Oders, setOrders] = useState([]);
+  const[form] = useForm();
+  const [selectedOrder, setSelectedOrder] =useState(null);
   const fetchHistory = async () => {
     try {
       const response = await api.get("/orders/history");
@@ -15,6 +19,21 @@ function History() {
   useEffect(() => {
     fetchHistory();
   }, []);
+
+  const handleFeedback= async (values) =>{
+  values.postId= selectedOrder.id;
+ 
+ try{
+     const response = await api.post("feedback",values);
+     fetchHistory();
+     setSelectedOrder(null);
+     form.resetFields();
+     toast.success("Succesfully created feedback");
+    } catch(e){
+      console.log(e)
+    }
+
+  }
   const columns = [
     {
       title: "Order ID",
@@ -37,11 +56,53 @@ function History() {
       dataIndex: "description",
       key: "description",
     },
+    {
+      title:"FeedBack",
+      dataIndex:"feedback",
+      key:"feedback",
+      render:(feedback, record) => record 
+    },
+    {
+      title: "Action",
+      dataIndex: "id",
+      key: "id",
+      render: (value, record) =>{
+        return( <Button type="primary" onClick={() => {
+          setSelectedOrder(record)
+        }}
+        >
+          Feedback
+          
+          </Button>
+        )
+      }
+    },
+    
   ];
   return (
     <div className="history">
       Lịch Sử Mua Hàng
       <Table dataSource={Oders} columns={columns} />
+      <Modal title="Feedback" open={selectedOrder} onOk={()=> form.submit()} onCancel={()=>{
+        setSelectedOrder(null)
+        }}
+        >
+      <Alert message={`Feedback cho don hang ${selectedOrder?.id}`} type="info" />
+        
+        <Form labelCol={{
+          span: 25,
+        }}
+        onFinish={handleFeedback}
+        form={form}
+        >
+          <Form.Item label="Rating" name="rating">
+      <Rate />
+      </Form.Item>
+      <Form.Item label="Feedback" name="content">
+      <Input.TextArea />
+      </Form.Item>
+      </Form>
+      </Modal>
     </div>
   );
 }
--- src/page/success/index.jsx
+++ src/page/success/index.jsx
@@ -3,10 +3,32 @@ import { Button, Result } from "antd";
 import { useNavigate } from "react-router-dom";
 import LOGO from "../../assets/images/logo.jpg";
 import "./index.scss";
+import api from "../../config/axios";
+import useGetParams from "../../assets/hook/useGetParams";
 
 function SuccessPage() {
   const navigate = useNavigate();
+  const params= useGetParams();
+  const orderID =params("orderID");
+  const vnp_TransactionStatus = params("vnp_TransactionStatus");
+  console.log(orderID);
+  console.log("Status:", vnp_TransactionStatus)
 
+  const postOrderID = async() => {
+    try{
+      const response = await api.post( `/orders/update-post-count/${orderID}`);
+  }catch(e){
+    console.log(e);
+  }
+  };
+
+  useEffect(()=>{
+    if(vnp_TransactionStatus ==="00"){
+      postOrderID();
+    }else{
+      
+    }
+  },[]);
   return (
     <div className="container">
       <div className="logo">
